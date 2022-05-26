acb9b2535d27fc145e044050a059792cd9bdcb90
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-26 13:50:05 +0700

    phan tich cu phap requests den

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always run');
- next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-    res.send('<h1>The Add product page</h1>')
-  });
-  app.use('/', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express.js</h1>');
+    res.send( "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>")
   });
 
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express.js</h1>");
+});
 
 app.listen(3000);
