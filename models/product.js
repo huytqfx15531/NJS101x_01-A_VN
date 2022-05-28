7bc9cdfb14cf52438b7893e6d2fe05021c30cec9
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-28 16:04:47 +0700

    Cau truc lai code luu tru file

--- models/product.js
+++ models/product.js
@@ -1,5 +1,21 @@
 const fs = require( 'fs' )
-const path = require('path')
+const path = require( 'path' )
+const p = path.join( path.dirname( process.mainModule.filename ), 'data', 'product.json' )
+
+
+const getProductsFromFile = cb =>
+{
+        fs.readFile( p, ( err, fileContent ) =>
+        { 
+            if ( err )
+            {
+                cb([])
+            } else
+            {
+                cb(JSON.parse(fileContent))
+            }
+        })
+}
 
 module.exports = class Product
 {
@@ -10,33 +26,19 @@ module.exports = class Product
     
     save ()
     {
-        const p = path.join( path.dirname( process.mainModule.filename ),'data','product.json' )
-        fs.readFile( p, ( err, fileContent ) => {
-            let products = []
-            if ( !err )
-            {
-                products = JSON.parse(fileContent)
-            }
+        getProductsFromFile( products =>
+        {
             products.push( this )
+
             fs.writeFile( p, JSON.stringify( products ), err =>
             {
                 console.log(err)
             })
-        }
-        )
-        
+        })
     }
 
     static fetchAll (cb)
     {
-        const p = path.join( path.dirname( process.mainModule.filename ), 'data', 'product.json' )
-        fs.readFile( p, ( err, fileContent ) =>
-        { 
-            if ( err )
-            {
-                cb([])
-            }
-            cb(JSON.parse(fileContent))
-        })
+        getProductsFromFile(cb)
     }
 }
