8fac076d58550865c7008a49cd1a5bc345fd9a10
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-28 15:38:27 +0700

    Them model (san pham & produce)

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require( '../models/product' )
 
 exports.getAddProduct = ( req, res, next ) =>
 {
@@ -12,11 +12,14 @@ exports.getAddProduct = ( req, res, next ) =>
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push( { title: req.body.title } );
-  res.redirect("/");
+    const product = new Product( req.body.title )
+    product.save()
+    res.redirect( "/" );
 }
 
-exports.getProduct = (req, res, next) => {
+exports.getProduct = ( req, res, next ) =>
+{
+    const products = Product.fetchAll()
     res.render( "shop", {
         prods: products,
         pageTitle: "Shop",
