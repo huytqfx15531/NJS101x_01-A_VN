9d3fdf9ba4a0bdfd8af5b0a8ecdae349d964a9f2
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-28 15:28:45 +0700

    Them Controller

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.router);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,10 @@
 const path = require("path");
 const express = require("express");
-const rootDir = require("../util/path");
+const productController = require('../controllers/products')
 const router = express.Router();
-const products = [];
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    activeAddProduct: true,
-    formsCSS: true,
-    productCSS: true
-  });
-});
+router.get("/add-product", productController.getAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post("/add-product", productController.postAddProduct);
 
-exports.router = router;
-exports.products = products;
+module.exports = router;
