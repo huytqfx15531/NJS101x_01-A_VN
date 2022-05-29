abb41e783aa1d023363e49cdb294551e01c83ff6
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-29 16:37:43 +0700

    them id san pham vao duong dan

--- models/product.js
+++ models/product.js
@@ -1,44 +1,41 @@
-const fs = require( 'fs' )
-const path = require( 'path' )
-const p = path.join( path.dirname( process.mainModule.filename ), 'data', 'product.json' )
+const fs = require('fs');
+const path = require('path');
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = cb =>
-{
-        fs.readFile( p, ( err, fileContent ) =>
-        { 
-            if ( err )
-            {
-                cb([])
-            } else
-            {
-                cb(JSON.parse(fileContent))
-            }
-        })
-}
-
-module.exports = class Product
-{
-    constructor ( t )
-    {
-        this.title = t
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    
-    save ()
-    {
-        getProductsFromFile( products =>
-        {
-            products.push( this )
+  });
+};
 
-            fs.writeFile( p, JSON.stringify( products ), err =>
-            {
-                console.log(err)
-            })
-        })
-    }
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    static fetchAll (cb)
-    {
-        getProductsFromFile(cb)
-    }
-}
+  save() {
+    this.id = Math.random()
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,19 @@
-const path = require("path");
-const express = require("express");
-const router = express.Router();
-const productController = require('../controllers/products')
+const path = require('path');
 
-router.get("/", productController.getProduct);
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
