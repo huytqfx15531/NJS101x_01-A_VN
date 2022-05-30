becdd65a85153ebd159e8f2c38eed855368b3233
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-30 14:37:45 +0700

    Xoa cac muc trong gio hang

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const path = require("path");
+const path = require( "path" );
+const Cart = require('./cart')
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -51,11 +52,20 @@ module.exports = class Product {
 
   static deleteById ( id )
   {
-    const updatedProducts = products.filter( prod => prod.id !== id )
-    fs.writeFile( p, JSON.stringify( updatedProducts ), err =>
-    {
-      console.log(err)
+    getProductsFromFile( ( products ) =>
+    { 
+      const product = products.find(prod=>prod.id===id)
+      const updatedProducts = products.filter( prod => prod.id !== id )
+      fs.writeFile( p, JSON.stringify( updatedProducts ), err =>
+      {
+        if ( !err )
+        {
+          Cart.deleteProduct(id, product.price)
+        }
+      })
+      
     })
+
   }
 
   static fetchAll(cb) {
