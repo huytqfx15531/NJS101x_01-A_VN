cfefb1e85cae5ba846f709eacc4fd763778fa107
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-30 12:42:23 +0700

    Dien truoc du lieu vao Trang chinh sua San pham

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,11 @@
+const { redirect } = require('express/lib/response');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false,
   });
 };
 
@@ -22,10 +24,17 @@ exports.getEditProduct = (req,res,next)=>{
   if(!editMode){
     return res.redirect('/')
   }
-  res.render('admin/edit-product',{
-    pageTitle: 'Edit Product',
-    path:'/admin/edit-product',
-    editing: editMode
+  const prodId = req.params.productId
+  Product.findById(prodId, product => {
+    if(!product) {
+      return res.redirect('/')
+    }
+    res.render('admin/edit-product',{
+      pageTitle: 'Edit Product',
+      path:'/admin/edit-product',
+      editing: editMode,
+      product: product
+  })
   })
 }
 
