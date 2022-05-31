ed4338dd258f7ec1c64ebb571387a1f4a45391b1
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-31 15:27:45 +0700

    SQL co ban va tao bang

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 
 const app = express();
+db.execute("SELECT * FROM products").then().catch();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
