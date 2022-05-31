0762ac0ac9f9b653f8f7d93d0cfacbf4a8152e98
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-05-31 16:47:07 +0700

    Update tim nap san pham

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  database: "node_complete",
-  password: "",
+  database: "node-complete",
+  password: "040199",
 });
 
 module.exports = pool.promise();
