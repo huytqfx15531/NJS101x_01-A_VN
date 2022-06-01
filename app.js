5e9b3d46058d0ccbb64591012b01b4f57b2b0f80
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-06-01 12:21:30 +0700

    Tao va quan ly nguoi dung tam thoi

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 const errorController = require("./controllers/error");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -11,19 +13,41 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const req = require("express/lib/request");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraint: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
   .sync()
-  .then((res) => {
-    /* console.log(res); */
+  .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Huy", email: "tranquanghuy.hoian04011999@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => console.log(err));
