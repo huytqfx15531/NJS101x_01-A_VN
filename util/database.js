8b8a163959d71c9963f45070bcfc14441190fa62
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-06-03 22:04:15 +0700

    Tong ket bai hoc ket noi co so du lieu

--- util/database.js
+++ util/database.js
@@ -1,14 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
     "mongodb+srv://Huyfx15531:040199@cluster0.xadso29.mongodb.net/?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected");
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
 };
 
 module.exports = mongoConnect;
+exports.getDb = getDb;
