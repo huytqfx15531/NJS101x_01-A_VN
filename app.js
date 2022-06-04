d4c01c215aa3d135af36b92087eabcea4a1a160a
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-06-04 20:08:45 +0700

    Them chuc nang them vao gio hang

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("629b56d3d75e5ff62bf19e1a")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
--- models/user.js
+++ models/user.js
@@ -3,10 +3,11 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email, cart) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
     this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -18,7 +19,9 @@ class User {
     // const cartProduct = this.cart.items.findIndex((cp) => {
     //   return cp._id === product_id;
     // });
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const updatedCart = {
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+    };
     const db = getDb();
     return db
       .collection("users")
