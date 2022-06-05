b50028840af0898ff2c71f7675fbfa7936c90a0a
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-06-05 14:21:51 +0700

    Ket noi voi may chu MongoDB bang Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("629b56d3d75e5ff62bf19e1a")
+  User.findById("616b065ceee6ba159464534d")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
@@ -30,6 +29,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://Huyfx15531:040199@cluster0.xadso29.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
