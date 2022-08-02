75db21935ad7b0d193b759925259c6a78f435640
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-08-02 21:56:59 +0700

    Lab8.8

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+
+const User = require("./models/user");
+
+const MONGODB_URI =
+  "mongodb+srv://Huyfx15531:040199@cluster0.xadso29.mongodb.net/?retryWrites=true&w=majority";
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRouters = require("./routes/auth");
 const errorController = require("./controllers/error");
-const User = require("./models/user");
-
-const MONGODB_URI =
-  "mongodb+srv://Huyfx15531:040199@cluster0.xadso29.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 const store = new MongoDBStore({
@@ -33,15 +35,6 @@ app.use(
     store: store,
   })
 );
-
-app.use((req, res, next) => {
-  User.findById("629cc7bc972d82932d39ff91")
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
