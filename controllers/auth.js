d9393e96f485a33c4605fc1b95d882b633845e21
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-08-03 19:03:17 +0700

    Lab9.1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcryptjs"); // cài đặt gói bcryptjs qua npm và import vào để mã hóa mật khẩu trong mongodb compass. Sử dụng dưới dòng 43, 45, 48 của postSignup
+const bcrypt = require("bcryptjs"); // cài đặt gói bcryptjs qua npm và import vào để mã hóa mật khẩu trong mongodb compass.
+const { validationResult } = require("express-validator/check");
 
 const User = require("../models/user");
 
@@ -64,6 +65,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Sign Up",
+      errorMessage: errors.array(),
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
 
@@ -9,7 +10,7 @@ router.post("/login", authController.postLogin);
 
 router.get("/signup", authController.getSignup);
 
-router.post("/signup", authController.postSignup);
+router.post("/signup", check("email").isEmail(), authController.postSignup);
 
 router.post("/logout", authController.postLogout);
 
