078f415181f588bd420d4d09322624c28d99d968
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-08-03 18:06:32 +0700

    Lab8.12

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,13 +9,42 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("629cc7bc972d82932d39ff91")
+  User.findById("626a667513c1b912e6192fed")
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
       req.session.save((err) => {
         console.log(err);
         res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Sign Up",
+    isAuthenticated: false,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { item: [] },
+      });
+      return user.save().then((result) => {
+        res.redirect("/login");
       });
     })
     .catch((err) => console.log(err));
