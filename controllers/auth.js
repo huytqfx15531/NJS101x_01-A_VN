d7185be033defc48b4a5bcaf7e3d9b680c86b9cf
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-08-03 18:57:13 +0700

    Lab8.24

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,6 +36,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
+          req.flash("error", "Invalid email or password.");
           res.redirect("/login");
         })
         .catch((err) => {
@@ -46,10 +47,16 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Sign Up",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -60,6 +67,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash(
+          "error",
+          "E-mail exists already, please pick a different one."
+        );
         return res.redirect("/signup");
       }
       return bcrypt
