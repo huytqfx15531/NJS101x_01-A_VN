d48b81e8e934b0a5185d3bc39b352b1541b6e4c5
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-08-04 17:43:57 +0700

    Lab10.21

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,7 +4,7 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -33,11 +33,11 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1;
   let totalItems;
 
   Product.find()
-    .count()
+    .countDocuments()
     .then((numProducts) => {
       console.log("numProducts", numProducts);
       totalItems = numProducts;
@@ -50,7 +50,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: "Shop",
         path: "/",
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
